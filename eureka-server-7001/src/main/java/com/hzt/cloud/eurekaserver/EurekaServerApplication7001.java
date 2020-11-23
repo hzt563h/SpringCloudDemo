2ca45ef58a2d9ea36dd1a8c608d152c29079b1eb
黄泽涛 <huangzetao@zhuojianchina.com>
2020-11-23 20:22:51 +0800

    eureka服务注册7001,7002集群

--- eureka-server-7001/src/main/java/com/hzt/cloud/eurekaserver/EurekaServerApplication7001.java
+++ eureka-server-7001/src/main/java/com/hzt/cloud/eurekaserver/EurekaServerApplication7001.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication7001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication7001.class, args);
     }
 
 }
---
+++ eureka-server-7002/src/test/java/com/hzt/cloud/eurekaserver/EurekaServerApplication7002Tests.java
@@ -0,0 +1,13 @@
+package com.hzt.cloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplication7002Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
