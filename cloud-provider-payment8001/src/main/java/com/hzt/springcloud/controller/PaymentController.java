b00a3dfcfd22ce0743ddf0e522d53d4dc9883c4e
hzt <1457182556@qq.com>
2020-11-26 22:43:02 +0800

    插入接口

--- cloud-provider-payment8001/src/main/java/com/hzt/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/hzt/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ package com.hzt.springcloud.controller;
 import com.hzt.springcloud.service.PaymentService;
 import com.hzt.springcloud.CommonResult;
 import com.hzt.springcloud.Payment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,7 @@ import javax.annotation.Resource;
  * @createTime 2020年11月24日 19:32:00
  */
 @RestController
+@Slf4j
 public class PaymentController {
 
     @Resource
@@ -25,6 +27,16 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****插入结果："+result);
+        if (result>0){  //成功
+            return new CommonResult(200,"插入数据库成功",result);
+        }else {
+            return new CommonResult(444,"插入数据库失败",null);
+        }
+    }
 
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
