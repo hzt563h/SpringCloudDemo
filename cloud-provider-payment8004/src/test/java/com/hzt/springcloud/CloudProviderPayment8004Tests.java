41bc4d15535ed22d9f15c60f494a97006c08a5e1
黄泽涛 <huangzetao@zhuojianchina.com>
2020-11-27 17:13:18 +0800

    CloudProviderPayment8004注册zookeeper

---
+++ cloud-provider-payment8004/src/test/java/com/hzt/springcloud/CloudProviderPayment8004Tests.java
@@ -0,0 +1,13 @@
+package com.hzt.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProviderPayment8004Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
