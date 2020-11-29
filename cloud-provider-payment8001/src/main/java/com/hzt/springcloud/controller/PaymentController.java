e5fbfec5a5de79d3d2a6437d1c8906e7f54e9f7e
hzt <1457182556@qq.com>
2020-11-29 13:36:02 +0800

    OpenFeign超时控制,OpenFeign日志打印功能

--- cloud-provider-payment8001/src/main/java/com/hzt/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/hzt/springcloud/controller/PaymentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author hzt
@@ -65,6 +66,12 @@ public class PaymentController {
             log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
         return this.discoveryClient;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try { TimeUnit.SECONDS.sleep(3); }catch (Exception e) {e.printStackTrace();}
+        return serverPort;
     }
 
 }
