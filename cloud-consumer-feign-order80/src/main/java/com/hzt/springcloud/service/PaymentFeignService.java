82a6c3b08c76c3bb8e741c90060ebbed7fe30064
hzt <1457182556@qq.com>
2020-11-29 13:19:42 +0800

    cloud-consumer-feign-order80-OpenFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/hzt/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,16 @@
+package com.hzt.springcloud.service;
+
+
+import com.hzt.springcloud.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id);
+}
