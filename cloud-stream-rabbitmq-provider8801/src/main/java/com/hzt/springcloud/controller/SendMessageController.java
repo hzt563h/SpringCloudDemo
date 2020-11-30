a3d820c99ee997eeb25f70f5eb39c09b928623e3
黄泽涛 <huangzetao@zhuojianchina.com>
2020-11-30 14:50:00 +0800

    SpringCloud Stream消息驱动

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/hzt/springcloud/controller/SendMessageController.java
@@ -0,0 +1,22 @@
+package com.hzt.springcloud.controller;
+
+import com.hzt.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+
+@RestController
+public class SendMessageController
+{
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage()
+    {
+        return messageProvider.send();
+    }
+
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/hzt/springcloud/service/IMessageProvider.java
@@ -0,0 +1,14 @@
+package com.hzt.springcloud.service;
+
+/**
+ * @author huangzetao
+ * @version 1.0.0
+ * @ClassName IMessageProvider.java
+ * @Description TODO
+ * @createTime 2020年11月30日 13:38:00
+ */
+public interface IMessageProvider
+{
+    public String send();
+}
+
